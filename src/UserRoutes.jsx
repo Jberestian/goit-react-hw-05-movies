1d9383d0f23d60a215f9a31c8361541dc210ca69
Jenia Berestian <jberestian@gmail.com>
2022-09-09 16:11:20 +0300

    added routes

--- src/UserRoutes.jsx
+++ src/UserRoutes.jsx
@@ -1,15 +1,26 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import Loader from 'shared/components/Loader/Loader';
+
 const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const SearchPage = lazy(() => import('pages/SearchPage/SearchPage'));
+const MovieInfo = lazy(() => import('pages/MovieInfo/MovieInfo'));
 const CastPage = lazy(() => import('pages/CastPage/CastPage'));
+const ReviewsPage = lazy(() => import('pages/ReviewsPage/ReviewsPage'));
+const Error = lazy(() => import('pages/Error/Error'));
 
 const UserRoutes = () => {
   return (
-    <Suspense>
+    <Suspense fallback={<Loader />}>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="cast" element={<CastPage />}></Route>
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/movies/:movieId" element={<MovieInfo />}>
+          <Route path="cast" element={<CastPage />} />
+          <Route path="reviews" element={<ReviewsPage />} />
+        </Route>
+        <Route path="*" element={<Error />} />{' '}
       </Routes>
     </Suspense>
   );
