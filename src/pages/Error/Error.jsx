544a5171815f1950c510683d801b9f0ac66aaad3
Jenia Berestian <jberestian@gmail.com>
2022-09-10 23:14:48 +0300

    added movie info functionality

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -8,7 +8,38 @@ const Error = () => {
     navigate(-1);
   };
 
-  return <></>;
+  return (
+    <div className={style.body}>
+      <div className={style.main}>
+        <div className={style.links}>
+          <button className={style.link} onClick={goBack}>
+            Go Back
+          </button>
+          <Link className={style.link} to="/">
+            Home
+          </Link>
+          <Link className={style.link} to="/search">
+            Search
+          </Link>
+          <Link className={style.link} to="/weekly-trending">
+            WeekPage
+          </Link>
+        </div>
+        <h1 className={style.title}>
+          4
+          <span className={style.span}>
+            <span className={style.eye}></span>
+            <span className={style.eye}></span>
+          </span>
+          4
+        </h1>
+        <h2 className={style.subtitle}>Error: 404 page not found</h2>
+        <p className={style.p}>
+          Sorry, the page you're looking for cannot be accessed
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
---
+++ src/pages/MovieInfo/MovieInfo.jsx
@@ -0,0 +1,111 @@
+import { useEffect, useState } from 'react';
+import {
+  useParams,
+  useNavigate,
+  Outlet,
+  Link,
+  useLocation,
+} from 'react-router-dom';
+
+import style from './movieInfo.module.css';
+
+import { getMovieDedails } from 'shared/api/movieApi';
+import Loader from 'shared/components/Loader/Loader';
+import Error from 'pages/Error/Error';
+
+const MovieDetails = () => {
+  const { movieId } = useParams();
+
+  const [state, setState] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const location = useLocation();
+
+  const from = location.state?.from || '/';
+
+  const navigate = useNavigate();
+
+  const goBack = () => navigate(from);
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const result = await getMovieDedails(movieId);
+        setState(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDetails();
+  }, [movieId]);
+
+  const { name, title, vote_average, overview, genres, poster_path } = state;
+
+  const generList =
+    genres &&
+    genres.map(({ name }, idx) => (
+      <li className={style.item} key={idx}>
+        {name}
+      </li>
+    ));
+
+  return (
+    <>
+      <div className={style.details}>
+        <div className="container">
+          {loading && <Loader />}
+          {error && <Error />}
+          <button onClick={goBack}>Go Back</button>
+          {state && (
+            <div className={style.info}>
+              <div className={style.imgBox}>
+                <img
+                  className={style.img}
+                  src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+                  alt="img"
+                />
+              </div>
+
+              <div className={style.about}>
+                <h1 className={style.title}>{name || title}</h1>
+                <p className={style.score}>User Score: {vote_average}</p>
+                <p className={style.overview}>Overview</p>
+                <p className={style.overviewText}>{overview}</p>
+                <p className={style.genres}>Genres</p>
+                <ul className={style.generList}>{generList}</ul>
+
+                <p className={style.add}>Additional Information</p>
+
+                <div className={style.links}>
+                  <Link
+                    state={from}
+                    className={style.link}
+                    to={`/movies/${movieId}/cast`}
+                  >
+                    Cast
+                  </Link>
+                  <Link
+                    state={from}
+                    className={style.link}
+                    to={`/movies/${movieId}/reviews`}
+                  >
+                    Reviews
+                  </Link>
+                </div>
+              </div>
+            </div>
+          )}
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MovieDetails;
