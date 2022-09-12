a08903eb28c480123ca572d69c3d9dd73b6c9914
Jenia Berestian <jberestian@gmail.com>
2022-09-12 09:22:21 +0300

    added fields

---
+++ src/pages/SearchPage/SearchPage.jsx
@@ -0,0 +1,14 @@
+// import { useState, useEffect } from 'react';
+// import { useSearchParams } from 'react-router-dom';
+
+// import TextField from 'shared/components/TextField/TextField';
+// import Loader from 'shared/components/Loader/Loader';
+// import MovieList from 'shared/components/MovieList/MovieList';
+
+// import Error from 'pages/Error/Error';
+
+// import { getSearchMovies } from 'shared/api/movieApi';
+
+// import fields from './fields';
+
+// import style from './searchPage.module.css';
---
+++ src/pages/SearchPage/fields.js
@@ -0,0 +1,12 @@
+const fields = {
+  search: {
+    type: 'text',
+    name: 'q',
+    placeholder: 'Search Movies',
+    required: true,
+    autoFocus: true,
+    autoComplete: 'off',
+  },
+};
+
+export default fields;
